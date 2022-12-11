763fc505030b6f037cd44bce70bc95d0477dc786
grubeben <gr.game.98@gmail.com>
2022-12-11 15:02:03 +0100

    updated discrete framework: normalization, initializer, activation_f options

--- a2cagent.py
+++ a2cagent.py
@@ -5,8 +5,8 @@ import tensorflow as tf
 import tensorflow_addons as tfa
 from tensorflow import keras
 from keras import layers
-from datetime import datetime
 
+from datetime import datetime
 import os
 import os.path
 import glob
@@ -18,11 +18,12 @@ class A2CAgent(keras.Model):
 
     """__CONSTRUCTOR__"""
 
-    def __init__(self,s0, act_dim, obs_dim, use_existing_policy=False):
+    def __init__(self, s0, act_dim, obs_dim, state_space_samples, state_normalization, batch_normalization, activation_function, initializer, use_existing_policy):
         super().__init__()
+
         """conveniece"""
         self.my_path = os.getcwd()
-        self.model_path=0
+        self.model_path = 0
 
         """parameters"""
         self.critic_loss_weight = 0.4
@@ -37,67 +38,80 @@ class A2CAgent(keras.Model):
         self.s0 = s0
 
         """ NN"""
+        self.metadata = {
+            'activation.functions': {'relu': keras.activations.relu, 'mish': tfa.activations.mish},
+            'initialization.functions': {'normal': keras.initializers.he_normal(), 'xavier': keras.initializers.glorot_normal()}
+        }
+
+        self.activation_function = self.metadata['activation.functions'][activation_function]
+        self.initializer = self.metadata['initialization.functions'][initializer]
+        self.state_normalization = state_normalization
+        self.state_space_samples = state_space_samples
+        self.batch_normalization = batch_normalization
+
         self.my_weight_dict = {}
-        self.my_weight_dict["dense1"] = layers.Dense(64, activation=tfa.activations.mish, kernel_initializer=keras.initializers.he_normal())
-        self.my_weight_dict["dense2"] = layers.Dense(64, activation=tfa.activations.mish, kernel_initializer=keras.initializers.he_normal())
+
+        # state normalisation
+        self.my_weight_dict["state_norm"] = layers.Normalization(axis=-1)
+        self.my_weight_dict["state_norm"].adapt(self.state_space_samples)
+
+        # main layers
+        self.my_weight_dict["dense1"] = layers.Dense(
+            64, activation=self.activation_function, kernel_initializer=self.initializer)
+        self.my_weight_dict["batch_norm"] = layers.BatchNormalization()
+        self.my_weight_dict["dense2"] = layers.Dense(
+            64, activation=self.activation_function, kernel_initializer=self.initializer)
         self.my_weight_dict["value"] = layers.Dense(1)
         self.my_weight_dict["policy_logits"] = layers.Dense(self.act_dim)
-
-        # self.dense1 = layers.Dense(
-        #     64, activation=tfa.activations.mish, kernel_initializer=keras.initializers.he_normal())
-        # self.dense2 = layers.Dense(
-        #     64, activation=tfa.activations.mish, kernel_initializer=keras.initializers.he_normal())
-        # # Instead of creating two entire NNs we just use one with two outlet channels 
-        # # only the last layer weights decide whether we obtain P(s,a) or V(s)
-        # self.value = layers.Dense(1)  # outlet 1
-        # self.policy_logits = layers.Dense(self.act_dim)  # outlet 2
 
     """__METHODS__"""
 
     """reward and advantages"""
-    def discounted_rewards_advantages(self,rewards,V_batch,V_nplus1,dones=0):
+
+    def discounted_rewards_advantages(self, rewards, V_batch, V_nplus1, dones=0):
         discounted_rewards = np.array(rewards + [V_nplus1])
         for t in reversed(range(len(rewards))):
-            discounted_rewards[t] = rewards[t] + self.gamma * discounted_rewards[t+1] * (1-dones[t])
+            discounted_rewards[t] = rewards[t] + self.gamma * \
+                discounted_rewards[t+1] * (1-dones[t])
         discounted_rewards = discounted_rewards[:-1]
         # advantages are bootstrapped discounted rewards - values, using Bellman's equation
         advantages = discounted_rewards - np.stack(V_batch)
         return discounted_rewards, advantages
 
     """losses"""
-    def critic_loss(self,discounted_rewards, predicted_values):
+
+    def critic_loss(self, discounted_rewards, predicted_values):
         return keras.losses.mean_squared_error(discounted_rewards, predicted_values) * self.critic_loss_weight
 
-    def entropy_loss(self,policy_logits):
-        return -(keras.losses.categorical_crossentropy(policy_logits,policy_logits, from_logits=True) * self.entropy_loss_weight)
+    def entropy_loss(self, policy_logits):
+        return -(keras.losses.categorical_crossentropy(policy_logits, policy_logits, from_logits=True) * self.entropy_loss_weight)
 
-    def actor_loss(self,combined, policy_logits):
+    def actor_loss(self, combined, policy_logits):
         actions = combined[:, 0]  # first column holds a_t
         advantages = combined[:, 1]  # second column holds A_t
-        """
-        plicy_loss = 0
-        for i in range(len(actions)):
-            policy_loss += advantages[i]*keras.losses.categorical_crossentropy(policy_logits[tf.cast(actions[i], tf.int32)])
-        """
         sparse_ce = keras.losses.SparseCategoricalCrossentropy(
-        from_logits=True, reduction=tf.keras.losses.Reduction.SUM)
+            from_logits=True, reduction=tf.keras.losses.Reduction.SUM)
         actions = tf.cast(actions, tf.int32)
-        policy_loss = sparse_ce(actions, policy_logits, sample_weight=advantages)
+        policy_loss = sparse_ce(actions, policy_logits,
+                                sample_weight=advantages)
 
-        return 0 #policy_loss * self.actor_loss_weight + self.entropy_loss(policy_logits) * self.entropy_loss_weight
+        return policy_loss * self.actor_loss_weight + self.entropy_loss(policy_logits) * self.entropy_loss_weight
 
     """NN methods"""
-    # def call(self, inputs):  # pass state forward through NN
-    #     x = self.dense1(inputs)
-    #     x = self.dense2(x)
-    #     return self.value(x), self.policy_logits(x)
 
     def call(self, inputs):  # pass state forward through NN
-        x = self.my_weight_dict["dense1"](inputs)
+        x = inputs
+        if (self.state_normalization == True):
+            x = self.my_weight_dict["state_norm"](x)
+        x = self.my_weight_dict["dense1"](x)
+        if (self.batch_normalization == True):
+            x = self.my_weight_dict["batch_norm"](x)
         x = self.my_weight_dict["dense2"](x)
+
         return self.my_weight_dict["value"](x), self.my_weight_dict["policy_logits"](x)
 
     def action_value(self, state):  # pass state through NN and choose action
         value, logits = self.predict_on_batch(state)  # runs call() from above
-        action = tf.random.categorical(logits, 1)[0] # choose random action based on action probabilities
+        # choose random action based on action probabilities
+        action = tf.random.categorical(logits, 1)[0]
         return action, value
--- train.py
+++ train.py
@@ -15,25 +15,32 @@ from tkinter.filedialog import askopenfilename
 import a2cagent
 
 
-def train(num_batches=10000, env_str="CartPole-v1", use_existing_policy=False, specification=""):
+def train(num_batches=10000, env_str="CartPole-v1", activation_function="relu", initializer="normal", state_normalization=False, batch_normalization=False, use_existing_policy=False, specification=""):
     # load environment
-    env = gym.make(env_str) 
+    env = gym.make(env_str)
     s0 = env.reset(return_info=False)
     s0 = env.reset()
-    
+
     obs_dim = env.observation_space.shape[0]
     act_dim = env.action_space
-    act_dim = 2
+    act_dim = 2  # has to be set manually
+
+    # state normalisation
+    state_space_samples = np.array(
+        [env.observation_space.sample() for x in range(6400)])
+    if (env_str == "CartPole-v1"):
+        # sample-method does not yield reasonable results for CartPole, so we load some historical state data
+        state_space_samples = np.loadtxt(os.getcwd()+"/obs-samples/norm_a.txt")
 
     # initiate agent
     model = a2cagent.A2CAgent(
-        s0, act_dim, obs_dim, use_existing_policy)
+        s0, act_dim, obs_dim, state_space_samples, state_normalization, batch_normalization, activation_function, initializer, use_existing_policy)
     # compile NN with inherited keras-method
     model.compile(optimizer=keras.optimizers.Adam(), loss=[
                   model.critic_loss, model.actor_loss])
+
     # load weights from existing model
     if use_existing_policy == True:
-        #print("\n\n")
         # string in format 'A2C281120221423CartPole-v1_mish'
         policy = input(
             "insert directory cooresponding to policy from which to start from:")
@@ -47,14 +54,14 @@ def train(num_batches=10000, env_str="CartPole-v1", use_existing_policy=False, s
         # model.load_weights(filename)
 
     # set up ModelCheckpoint
-    model.model_path = model.my_path+'/training/models' + \
+    model.model_path = model.my_path+'/training_discrete' + \
         f"/A2C{datetime.now().strftime('%d%m%Y%H%M')}" + \
-        env_str+'_'+specification+'/'
-    os.makedirs(model.model_path)
+        env_str+'_'+activation_function+'_'+initializer+'_'+specification
+    os.makedirs(model.model_path+'/model/')
 
     # set up TensorBoard to visualize progress
     train_writer = tf.summary.create_file_writer(
-        model.my_path + '/training/tensorboard'+f"/A2C{datetime.now().strftime('%d%m%Y%H%M')}"+env_str+"_"+specification)
+        model.model_path + '/tensorboard')
 
     # main training loop
     episode_reward_sum = 0
@@ -80,21 +87,22 @@ def train(num_batches=10000, env_str="CartPole-v1", use_existing_policy=False, s
             episode_reward_sum += reward
 
             s = s_new
-            #print(s)
 
             # handle end of episode
             if done:
                 rewards.append(0.0)
                 s = env.reset()
                 # loss will be 0 for first batch
-                print(f"Episode: {episode}, latest episode reward: {episode_reward_sum}, loss: {loss}")
+                print(
+                    f"Episode: {episode}, latest episode reward: {episode_reward_sum}, loss: {loss}")
                 with train_writer.as_default():
                     tf.summary.scalar('rewards', episode_reward_sum, episode)
 
                 # safe best model-version for later use
                 if (episode_reward_sum >= episode_reward_sum_best):
                     episode_reward_sum_best = episode_reward_sum
-                    model.save_weights(model.model_path, save_format="tf")
+                    model.save_weights(
+                        model.model_path+'/model/', save_format="tf")
 
                 episode_reward_sum = 0
                 episode += 1
@@ -110,13 +118,15 @@ def train(num_batches=10000, env_str="CartPole-v1", use_existing_policy=False, s
         combined[:, 1] = advantages
         # cperform NN update with keras method and obtain loss
         # discounted_rewards are used for the critic update, combined=[a_t, A_t] for the actor
-        #print("\n\n", tf.shape(tf.stack(states)),"\n\n")
-        #print("\n\n",combined,"\n\n")
         loss = model.train_on_batch(
             tf.stack(states), [discounted_rewards, combined], return_dict=False)
         with train_writer.as_default():
             tf.summary.scalar('tot_loss', np.sum(loss), step)
 
+
 if __name__ == "__main__":
-    # "CartPole-v1": set sct_dim=2 manually;  "CartPoleBulletEnv-v1": get rid of return_info arg; "HopperBulletEnv-v0"
-    train(num_batches=300, specification="mish", use_existing_policy=False)
+    """NOTE"""
+    # "CartPole-v1": set sct_dim=2 manually;
+
+    train(num_batches=300, specification="TEST", activation_function='mish', initializer='xavier', state_normalization=True,
+          batch_normalization=True, use_existing_policy=False)
